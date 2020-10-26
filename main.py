cf9172548eef398012641bbf2adb6f2d2d274b14
Charlie Kelson <charlie.kelson@gettyimages.com>
2020-10-25 19:30:05 -0700

    Add reqs for assignment 5

--- main.py
+++ main.py
@@ -14,15 +14,27 @@ def get_fact():
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
 
-    return facts[0].getText()
+    return facts[0].getText().strip()
+
+
+def get_pig_address(fact):
+    data = {'input_text': fact}
+    pig = requests.post('https://hidden-journey-62459.herokuapp.com'
+                        '/piglatinize/',
+                        data=data)
+    return pig.url
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    address = get_pig_address(fact)
+    return address
 
 
 if __name__ == "__main__":
+    # fact = get_fact()
+    # get_pig_address(fact)
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
 
